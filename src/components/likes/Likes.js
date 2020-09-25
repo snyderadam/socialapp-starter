f4069d813bbdbbbf769c16086c48f75942903edc
rose <wittyrosegarcia@yahoo.com>
2020-09-25 17:41:08 -0400

    add new addedcode

--- src/components/likes/Likes.js
+++ src/components/likes/Likes.js
@@ -1,14 +1,35 @@
 import axios from "axios";
 
+
 class Likes {
-    constructor(url="https://socialapp-api.herokuapp.com/",client = axios.create()){
+    constructor (url="https://socialapp-api.herokuapp.com/",client = axios.create()){
         this.url = url;
         this.client = client;
     }
+    getPreviousMessage(){
+        return this.client.get(this.url + "/messages<=20")
+        .then (response => {
+            if (this.client.response && ! response.error)
+            return response.data.messages
+        })
+
+    }
+
     getLikesFunction(){
-        return this.client.post(this.url = "/likes");
+        return this.client.post(this.url + "/likes");
+    }
+    getToken(){
+        let login = JSON.parse(localStorage.getItem("login"))
+        let {token} = login.result
+        return token
+         
+        
+    }
+    postLikes(messageId){
+        let requestId = {messageId}
     }
         
     
 }
+
 export default Likes
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 class Message extends React.Component {
-    state = {likeThumb: this.props.likes.length}
-    handleLike = () => {
-        this.setState(latestState => ({likeThumb: latestState.likeThumb + 1}))
+    state = {likeThumb: this.props.likes.length,
+        likes:this.props.likes
     }
+    handleLike = () => {
+        
+    }
+        
+    removeLike(id) {this.setState({likeThumb: this.state.likeThumb.filter(likeThumb => likeThumb.id !== id)});
+    
+}
     render() { 
         return ( 
             <div>
@@ -13,13 +19,18 @@ class Message extends React.Component {
         {this.props.username}
         <div className="textDiv">{this.props.text}</div>
         <div className="like-Thumb">Likes: {this.state.likeThumb}</div>
+        
         <button onClick={this.handlesLike}>
-            <img class="image" src="https://opticalprism.ca/wp-content/uploads/2019/09/likes.png" alt="100" width="25px" height="25px" />
-             
+            <img className="image" src="https://opticalprism.ca/wp-content/uploads/2019/09/likes.png" alt="100" width="25px" height="25px" />
+           
         </button>
+        <button onClick={this.props.removed}>dislike</button>  
         </li></div>
          );
+
     }
+    
 }
  
 export default Message;
+//export default DataService
